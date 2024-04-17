07619de6e90791f358c06f4b18088ff06613f8c1
AnAsianGuyOnTheInternet <166057912+AnAsianGuyOnTheInternet@users.noreply.github.com>
2024-04-17 14:43:45 -0400

    /demoMinh

---
+++ Car.java
@@ -0,0 +1,16 @@
+public class Car {
+    private String name;
+    private int navigationTime;
+
+    public Car(String nameA, int navTime) {
+        name = nameA;
+        navigationTime = navTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getNavigationTime() {
+        return navigationTime;
+    }
+}
