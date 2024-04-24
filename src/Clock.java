25ecfd44a10544202e8382fb20c7bfe2b1c2177d
Dean Myers <dmyers5348@comcast.net>
2024-04-24 13:03:53 -0400

    Add Clock.java

---
+++ src/Clock.java
@@ -0,0 +1,10 @@
+public class Clock {
+    private double currentTime;
+    // TODO: Make List class
+    // private List eventList
+
+    Clock() {
+        currentTime = 0.0;
+        // eventList = new List();
+    }
+}
